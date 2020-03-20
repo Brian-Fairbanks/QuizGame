0f4a3a1111132455ab967903a7d773e939c89b7f
Brian Fairbanks <brian.k.fairbanks@gmail.com>
2020-03-20 16:50:38 -0500

    Added header, footer, and intro card to html.  Set up a few css classes.  set up variables I will be using later in JS

---
+++ scripts.js
@@ -0,0 +1,11 @@
+// ========== Global Variables ==========
+//========================================
+var timerFill = document.getElementById("#timerFill");
+var timerSpan = document.getElementById("#timerSpan");
+
+// ========== Functions =================
+//========================================
+
+
+// ========== Main ======================
+//========================================
