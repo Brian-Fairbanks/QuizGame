defe9b59bb10974cf479a5870ad9676d0a1713a4
Brian Fairbanks <brian.k.fairbanks@gmail.com>
2020-03-20 16:59:15 -0500

    Added Questionaire card, and set up associated spans as javascript variables

--- scripts.js
+++ scripts.js
@@ -3,6 +3,10 @@
 var timerFill = document.getElementById("#timerFill");
 var timerSpan = document.getElementById("#timerSpan");
 
+var questionSpan = document.getElementById("#questionSpan");
+var choiceSpan = document.getElementById("#choiceSpan");
+var answerSpan = document.getElementById("#answerSpan");
+
 // ========== Functions =================
 //========================================
 
