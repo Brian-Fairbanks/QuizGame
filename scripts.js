ef220e6d477bd0575c8509fc7e98c60fd871f8e5
Brian Fairbanks <brian.k.fairbanks@gmail.com>
2020-03-20 17:23:29 -0500

    Added results card, and associated span/& buttons to JS variables.

--- scripts.js
+++ scripts.js
@@ -3,9 +3,17 @@
 var timerFill = document.getElementById("#timerFill");
 var timerSpan = document.getElementById("#timerSpan");
 
+var introCard = document.getElementById("#intro");
+var startBtn = document.getElementById("#startBtn");
+
+var questionCard = document.getElementById("#questions");
 var questionSpan = document.getElementById("#questionSpan");
 var choiceSpan = document.getElementById("#choiceSpan");
 var answerSpan = document.getElementById("#answerSpan");
+
+var resultsCard = document.getElementById("#results");
+var scoreSpan = document.getElementById("#scoreSpan");
+var retryBtn = document.getElementById("#retryBtn");
 
 // ========== Functions =================
 //========================================
